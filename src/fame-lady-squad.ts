5f8b39eea1e045b1b6a3ec7bcb46446b00a36800
subgraphdev <ujjwaltha065@gmail.com>
2023-03-08 10:47:43 +0530

    intial commit

--- src/fame-lady-squad.ts
+++ src/fame-lady-squad.ts
@@ -1,73 +1,73 @@
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
   Transfer as TransferEvent
-} from "../generated/FameLadySquad/FameLadySquad"
+} from "../generated/FameLadySquad/FameLadySquad";
+
 import {
-  Approval,
-  ApprovalForAll,
-  OwnershipTransferred,
-  Transfer
-} from "../generated/schema"
+  Lady, User
+} from "../generated/schema";
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
+import { log, ipfs, json, JSONValue } from "@graphprotocol/graph-ts";
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+const ipfsHash = "QmTngWTnURuyiz1gtoY33FKghCiU2uQusXpnUc36QJNKsY";
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
+  let lady = Lady.load(event.params.tokenId.toString());
+  if (lady == null) {
+    lady = new Lady(event.params.tokenId.toString());
+    lady.tokenID = event.params.tokenId;
+    lady.tokenURI = "/" + event.params.tokenId.toString();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    let metadata = ipfs.cat(ipfsHash + lady.tokenURI);
+    if (metadata) {
+      const value = json.fromBytes(metadata).toObject();
+      if (value) {
+        const name = value.get("name");
 
-  entity.save()
+        if (name) {
+          lady.name = name.toString();
+        }
+      }
+
+      let atttributes: JSONValue[];
+      let ladyAttributes = value.get("attributes");
+      if (ladyAttributes) {
+        atttributes = ladyAttributes.toArray();
+
+        for (let i = 0; i < atttributes.length; i++) {
+          let item = atttributes[i].toObject();
+          let trait: string;
+          let traitName = item.get("trait_type");
+          if (traitName) {
+            trait = traitName.toString();
+            let value: string;
+            let traitValue = item.get("value");
+            if (traitValue) {
+              value = traitValue.toString();
+              if(trait == "Hair"){
+                lady.hairStyle = value;
+              }
+              if(trait == "Skin"){
+                lady.skinColor = value;
+              }
+              if(trait== "Eyes"){
+                lady.eyeColor = value;
+              }
+              if(trait == "Face Expression"){
+                lady.faceExpression = value;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  lady.owner = event.params.to.toHexString();
+  lady.save;
+
+  let user = User.load(event.params.to.toHexString());
+  if(!user){
+    user = new User(event.params.to.toHexString());
+    user.save
+  }
 }
